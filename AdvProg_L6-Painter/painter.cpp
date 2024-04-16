0dfbaa0d18ee7e1eadbc082c5d03b5dbd6ee185d
HoangHaiUET <23020660@vnu.edu.vn>
2024-04-16 14:04:16 +0700

    fix

--- AdvProg_L6-Painter/painter.cpp
+++ AdvProg_L6-Painter/painter.cpp
@@ -54,7 +54,7 @@ void Painter::turnLeft(double degree)
 {
     // Rotate the painter left by the specified degree
     angle -= degree;
-    
+
     // Ensure the angle stays within the range of 0 to 360 degrees
     while (angle < 0) {
         angle += 360;
@@ -71,11 +71,11 @@ void Painter::turnLeft(double degree)
     Returns:
         None
 ***/
-void Painter::turnLeft(double degree)
+void Painter::turnRight(double degree)
 {
     // Rotate the painter left by the specified degree
     angle += degree;
-    
+
     // Ensure the angle stays within the range of 0 to 360 degrees
     while (angle < 0) {
         angle += 360;
